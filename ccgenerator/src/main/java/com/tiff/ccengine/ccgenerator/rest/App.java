51b4a8d3be787a0b2b90a848ada42d9dfee469fb
Hendre <mahesh_hendre@nuance.com>
2018-07-09 09:56:55 +0530

    modification

--- ccgenerator/src/main/java/com/tiff/ccengine/ccgenerator/rest/App.java
+++ ccgenerator/src/main/java/com/tiff/ccengine/ccgenerator/rest/App.java
@@ -3,7 +3,7 @@ package com.tiff.ccengine.ccgenerator.rest;
 import org.glassfish.jersey.server.ResourceConfig;
 /**
  *
- * @author skargopolov
+ * @author mahesh_hendre
  */
 public class App extends ResourceConfig{
     public App() {
